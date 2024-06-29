6fede2372f33863d55530de8629c55b3de71ec08
MuratOzte <mo2553481@gmail.com>
2024-06-29 22:30:24 +0300

    scroll button added

--- src/components/Hero/HeroMain.tsx
+++ src/components/Hero/HeroMain.tsx
@@ -5,8 +5,8 @@ import { motion } from 'framer-motion';
 
 const HeroMain = () => {
     return (
-        <div className="absolute z-30 top-[25%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-transparent px-4 py-2 backdrop-blur-md opacity-90 rounded-md">
-            <p className="text-white text-9xl">YZ TEXTILE</p>
+        <div className="absolute z-30 top-[25%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-transparent px-4 py-2 rounded-md drop-shadow-xl">
+            <p className="text-neutral-300 text-9xl brightness-200 font-semibold [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">YZ TEXTILE</p>
         </div>
     );
 };
--- src/components/Slide/ImageCarousel.tsx
+++ src/components/Slide/ImageCarousel.tsx
@@ -7,6 +7,7 @@ import image2 from '@/assets/slide/slide2.jpeg';
 import image3 from '@/assets/slide/slide3.jpeg';
 import image4 from '@/assets/slide/slide4.jpeg';
 import image5 from '@/assets/slide/slide5.jpeg';
+import { FaChevronDown } from 'react-icons/fa';
 import HeroMain from '../Hero/HeroMain';
 
 const images = [image1, image2, image3, image4, image5];
@@ -31,7 +32,19 @@ const ImageCarousel = () => {
 
     return (
         <div className="relative w-full h-[calc(100vh-64px)] overflow-hidden">
-            <HeroMain />
+            <motion.div
+                initial={{ y: 0 }}
+                animate={{ y: [0, 20, 0, 20, 0] }}
+                transition={{
+                    repeat: Infinity,
+                    duration: 3,
+                    ease: 'easeInOut',
+                }}
+                style={{ translateX: '-50%' }}
+                className="absolute z-30 top-[80%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-black p-2  rounded-3xl shadow-xl cursor-pointer hover:scale-110"
+            >
+                <FaChevronDown size={64} color="white" />
+            </motion.div>
             <div className="absolute top-0 left-0 w-full h-full">
                 {/* Transparent linear gradient */}
                 <div
@@ -39,6 +52,14 @@ const ImageCarousel = () => {
                     style={{
                         background:
                             'linear-gradient(0deg, rgba(0, 0, 0, 0) 90%, rgba(255,255, 255, 1) 100%)',
+                        zIndex: 1,
+                    }}
+                ></div>
+                <div
+                    className="absolute top-0 left-0 w-full h-full"
+                    style={{
+                        background:
+                            'linear-gradient(0deg, rgba(0, 0, 0, 0) 100%, rgba(0,0, 0, 0.5) 0%)',
                         zIndex: 1,
                     }}
                 ></div>
