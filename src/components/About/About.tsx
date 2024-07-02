6554fb184a70e841c5857696ed360b0c548f5569
MuratOzte <mo2553481@gmail.com>
2024-07-02 15:54:20 +0300

    refactor: Update delay values for animations in About component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -69,7 +69,7 @@ const About = () => {
                                             }}
                                             transition={{
                                                 duration: 1,
-                                                delay: 1,
+                                                delay: 0.5,
                                             }}
                                             className="text-slate-500 w-1/3 tracking-wider text-center text-lg"
                                         >
@@ -86,7 +86,7 @@ const About = () => {
                                             }}
                                             transition={{
                                                 duration: 1,
-                                                delay: 1,
+                                                delay: 0.5,
                                             }}
                                             className="rounded-md overflow-hidden  shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
                                         >
@@ -109,7 +109,7 @@ const About = () => {
                                             }}
                                             transition={{
                                                 duration: 1,
-                                                delay: 1.4,
+                                                delay: 0.7,
                                             }}
                                             className="rounded-md overflow-hidden shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
                                         >
@@ -129,7 +129,7 @@ const About = () => {
                                             }}
                                             transition={{
                                                 duration: 1,
-                                                delay: 1.4,
+                                                delay: 0.7,
                                             }}
                                             className="text-slate-500 w-1/3 tracking-wider text-center text-lg"
                                         >
