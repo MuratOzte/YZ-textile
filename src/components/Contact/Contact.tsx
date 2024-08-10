4aed005722690cfcf2c0bb237ef07534b93c1107
MuratOzte <mo2553481@gmail.com>
2024-08-10 21:57:30 +0300

    contact and sustainability responsivness added

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,21 +1,21 @@
 import { CiMail } from 'react-icons/ci';
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp } from 'react-icons/fa';
 
 const Contact = () => {
     return (
-        <div className="bg-gray-100 p-8 rounded-lg shadow-lg max-w-4xl mx-auto mt-16 mb-6">
+        <div className="bg-gray-100 p-8 rounded-lg shadow-lg max-w-4xl mx-auto mt-16 mb-8">
             <div className="text-center mb-8">
-                <h1 className="text-3xl font-semibold text-gray-800 mb-2">
+                <h1 className="text-3xl md:text-4xl font-semibold text-gray-800 mb-2">
                     Contact
                 </h1>
-                <p className="text-lg text-gray-600">
+                <p className="text-lg md:text-xl text-gray-600">
                     You can contact our 24/7 customer service to get prices and
                     information.
                 </p>
             </div>
-            <div className="flex flex-col md:flex-row justify-around items-center">
-                <div className="flex items-center mb-4 md:mb-0">
-                    <CiMail className="text-3xl text-gray-800 mr-3" />
+            <div className="flex flex-col sm:flex-row justify-center sm:justify-around items-center">
+                <div className="flex items-center mb-4 sm:mb-0 space-x-3">
+                    <CiMail className="text-3xl text-gray-800" />
                     <a
                         href="mailto:email@example.com"
                         className="text-lg text-gray-800 hover:text-gray-600"
@@ -23,8 +23,8 @@ const Contact = () => {
                         email@example.com
                     </a>
                 </div>
-                <div className="flex items-center">
-                    <FaWhatsapp className="text-3xl text-gray-800 mr-3" />
+                <div className="flex items-center space-x-3">
+                    <FaWhatsapp className="text-3xl text-gray-800" />
                     <a
                         href="https://wa.me/905313749574?text=Hello, I want to get information about clothing production"
                         className="text-lg text-gray-800 hover:text-gray-600"
--- src/components/Sustainability/Sustainability.tsx
+++ src/components/Sustainability/Sustainability.tsx
@@ -3,13 +3,13 @@ import Sustaibability from '@/assets/sustainability/sustainability.jpeg';
 
 const Sustainability = () => {
     return (
-        <div className="flex flex-col md:flex-row mx-4 md:mx-16 justify-center mt-16 items-center gap-12">
-            <div className="w-full md:w-1/3">
-                <div className="space-y-8 md:space-y-12 text-center md:text-left">
-                    <h2 className="text-4xl font-bold text-gray-800 text-center">
+        <div className="flex flex-col md:flex-row mx-4 md:mx-16 justify-center mt-16 items-center gap-8 md:gap-12">
+            <div className="w-full md:w-1/2 lg:w-1/3">
+                <div className="space-y-6 md:space-y-8 text-center md:text-left">
+                    <h2 className="text-3xl md:text-4xl font-bold text-gray-800">
                         Sustainability
                     </h2>
-                    <p className="text-xl leading-relaxed text-gray-600 text-center">
+                    <p className="text-lg md:text-xl leading-relaxed text-gray-600">
                         Sustainability is at the core of our journey forward. We
                         conscientiously evaluate our actions for their impact on
                         production, communities, and the environment, aiming to
@@ -21,7 +21,7 @@ const Sustainability = () => {
                     </p>
                 </div>
             </div>
-            <div className="w-full md:w-auto shadow-xl">
+            <div className="w-full md:w-auto flex justify-center">
                 <Image
                     alt="sustain"
                     src={Sustaibability}
